139dc7cfe5b5987a443bb3d9e1d611ff94e11193
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-01-07 18:45:33 +0500

    Init tree from react-family-tree

--- pages/tree.tsx
+++ pages/tree.tsx
@@ -1,5 +1,10 @@
 import type { NextPage } from "next";
+import Tree from "../components/Tree/Tree";
 
-const TreePage: NextPage = () => <div>Страница с деревом будет здесь</div>;
+const TreePage: NextPage = () => (
+  <div>
+    <Tree />
+  </div>
+);
 
 export default TreePage;
