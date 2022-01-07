36874f4e9b45c9d2df6f4a40c08717342194f7f0
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-01-07 18:45:33 +0500

    Init tree from react-family-tree

---
+++ components/Tree/Tree.tsx
@@ -0,0 +1,39 @@
+import dynamic from "next/dynamic";
+import { FC } from "react";
+import { ExtNode, Node } from "relatives-tree/lib/types";
+import familyNodes from "./data/family.json";
+import FamilyNode from "./FamilyNode/FamilyNode";
+import s from "./Tree.module.css";
+const ReactFamilyTree = dynamic(import("react-family-tree"), { ssr: false });
+
+const WIDTH = 70;
+const HEIGHT = 80;
+
+const nodes: Node[] = familyNodes as Node[];
+
+const rootId = nodes[0].id;
+
+const Tree: FC = () => (
+  <ReactFamilyTree
+    nodes={nodes}
+    rootId={rootId}
+    width={WIDTH}
+    height={HEIGHT}
+    className={s.root}
+    renderNode={(node: ExtNode) => (
+      <FamilyNode
+        key={node.id}
+        node={node}
+        style={{
+          width: WIDTH,
+          height: HEIGHT,
+          transform: `translate(${node.left * (WIDTH / 2)}px, ${
+            node.top * (HEIGHT / 2)
+          }px)`,
+        }}
+      />
+    )}
+  />
+);
+
+export default Tree;
