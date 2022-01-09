faacd9d59b33f90366c212750fce56bb7e8f5e68
Daria Artemova <airadavometra@gmail.com>
2022-01-09 12:59:18 +0300

    add main page img

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,28 +7,39 @@ import classNames from "classnames";
 
 const HomePage: NextPage = () => (
   <div className={s.pageContainer}>
-    <div className={s.descriptionContainer}>
-      <div className={classNames(s.logoContainer, s.descriptionItem)}>
-        <Image
-          src="/LogoBig.png"
-          width={120}
-          height={110}
-          alt="Логотип проекта древо"
-        />
-        <span className={s.logoTitle}>ДРЕВО</span>
+    <div className={s.content}>
+      <div className={s.descriptionContainer}>
+        <div className={classNames(s.logoContainer, s.descriptionItem)}>
+          <Image
+            src="/LogoBig.png"
+            width={120}
+            height={110}
+            alt="Логотип проекта древо"
+          />
+          <span className={s.logoTitle}>ДРЕВО</span>
+        </div>
+        <span className={classNames(s.description, s.descriptionItem)}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ex hic quo
+          est facere dignissimos esse minima tenetur consequatur, vitae
+          molestias tempora qui adipisci amet fugit sapiente deserunt accusamus
+          odit?
+        </span>
+        <Link href="/tree">
+          <button className={classNames(s.button, s.descriptionItem)}>
+            Посмотреть дерево
+          </button>
+        </Link>
       </div>
-      <span className={classNames(s.description, s.descriptionItem)}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ex hic quo
-        est facere dignissimos esse minima tenetur consequatur, vitae molestias
-        tempora qui adipisci amet fugit sapiente deserunt accusamus odit?
-      </span>
-      <Link href="/tree">
-        <button className={classNames(s.button, s.descriptionItem)}>
-          Посмотреть дерево
-        </button>
-      </Link>
+      <NodesCounter />
     </div>
-    <NodesCounter />
+    <div className={s.imageContainer}>
+      <Image
+        src="/LogoBig.png"
+        width={960}
+        height={880}
+        alt="Логотип проекта древо"
+      />
+    </div>
   </div>
 );
 
