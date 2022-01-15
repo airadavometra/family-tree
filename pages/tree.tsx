b047fc070c213b4a210215e218c7bf7eee0c307f
Daria Artemova <airadavometra@gmail.com>
2022-01-15 14:51:22 +0300

    add 404 redirect

--- pages/tree.tsx
+++ pages/tree.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import TreeNodeDetails from "../components/Tree/TreeNodeDetails/TreeNodeDetails";
 import TreeWithNavigation from "../components/Tree/TreeWithNavigation/TreeWithNavigation";
 import { TreeContextWrapper, useTreeRootId } from "../context/tree";
+import { getTreeNodesMap } from "../data";
 import s from "../styles/TreePage.module.css";
 
 const TreePage: NextPage = () => {
+  const router = useRouter();
   const { rootId } = useTreeRootId();
+
+  const treeNodesMap = getTreeNodesMap();
+
+  if (rootId && treeNodesMap[rootId] === undefined) {
+    router.push("/404");
+    return null;
+  }
 
   return (
     <TreeContextWrapper>
