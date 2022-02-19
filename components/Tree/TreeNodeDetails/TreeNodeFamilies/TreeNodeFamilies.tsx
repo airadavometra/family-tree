ff51edb71187cc34bb28743dbdd45170e57595c2
Daria Artemova <airadavometra@gmail.com>
2022-02-19 22:15:25 +0300

    fix families list overflow

--- components/Tree/TreeNodeDetails/TreeNodeFamilies/TreeNodeFamilies.tsx
+++ components/Tree/TreeNodeDetails/TreeNodeFamilies/TreeNodeFamilies.tsx
@@ -10,22 +10,26 @@ export const TreeNodeFamilies: FC<TreeNodeFamiliesProps> = ({ families, fullName
   const { rootId } = useUrlTreeRootId();
 
   return families.length ? (
-    <div className={s.familyLinksContainer}>
+    <>
       <span className={s.familyLinksTitle}>{`${fullName} является потомком семей:`}</span>
-      {families.map((family) => {
-        if (rootId === family.id) {
-          return (
-            <span className={classNames(s.selectedFamily, s.familyItem)}>{`${family.lastName} – открыта сейчас`}</span>
-          );
-        } else {
-          return (
-            <a href={`/tree?root=${family.id}`} className={classNames(s.familyLink, s.familyItem)}>
-              {family.lastName}
-            </a>
-          );
-        }
-      })}
-    </div>
+      <div className={s.familyLinksContainer}>
+        {families.map((family) => {
+          if (rootId === family.id) {
+            return (
+              <span
+                className={classNames(s.selectedFamily, s.familyItem)}
+              >{`${family.lastName} – открыта сейчас`}</span>
+            );
+          } else {
+            return (
+              <a href={`/tree?root=${family.id}`} className={classNames(s.familyLink, s.familyItem)}>
+                {family.lastName}
+              </a>
+            );
+          }
+        })}
+      </div>
+    </>
   ) : (
     <span className={s.familyLinksTitle}>{`${fullName} является корнем и не имеет других веток.`}</span>
   );
