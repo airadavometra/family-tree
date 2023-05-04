77d50c3f112c1f4b41f1bbf4310623fd0df70041
Daria Artemova <18581702+airadavometra@users.noreply.github.com>
2023-05-04 21:02:47 +0200

    change company name

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import s from "./Footer.module.css";
 
 const Footer: FC = () => (
   <footer className={s.footer}>
-    <span className={s.footerItem}>DIV DEV STUDIO</span>
+    <span className={s.footerItem}>WONDERCODE B.V.</span>
     <span className={s.footerItem}>2022</span>
   </footer>
 );
